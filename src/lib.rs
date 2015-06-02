92737c3844ca47bdc792316c0ec7aa6ad090d164
Andrew O'Neill <phasmid1@gmail.com>
2015-06-01 22:31:01 -0400

    Implemented new test case - playing a basic land

--- src/lib.rs
+++ src/lib.rs
@@ -6,17 +6,42 @@ impl Player {
     }
 
     pub fn play_turn(&self, prev_game_state: GameState) -> GameState {
-        GameState { turn: prev_game_state.turn + 1 }
+        GameState {
+            turn: prev_game_state.turn + 1,
+            battlefield: prev_game_state.battlefield,
+        }
     }
 }
 
 
 pub struct GameState {
     pub turn: i32,
+    pub battlefield: Vec<String>,
 }
 
 impl GameState {
     pub fn new() -> GameState {
-        GameState { turn: 0 }
+        GameState {
+            turn: 0,
+            battlefield: Vec::new(),
+        }
+    }
+
+    pub fn new_with_hand(h: Hand) -> GameState {
+        GameState {
+            turn: 0,
+            battlefield: vec![h.card],
+        }
+    }
+}
+
+
+pub struct Hand {
+    pub card: String,
+}
+
+impl Hand {
+    pub fn new(card_str: &str) -> Hand {
+        Hand { card: card_str.to_string() }
     }
 }
--- tests/test_simple_game.rs
+++ tests/test_simple_game.rs
@@ -1,11 +1,22 @@
 extern crate mtgsim;
 
-use mtgsim::{GameState, Player};
+use mtgsim::{GameState, Hand, Player};
 
 #[test]
-fn test_simple_game() {
+fn test_player_pass_turn() {
     let player = Player::new();
     let simple_game_state = GameState::new();
     let new_game_state = player.play_turn(simple_game_state);
-    assert_eq!(new_game_state.turn, 1);
+    assert_eq!(1, new_game_state.turn);
+}
+
+#[test]
+fn test_play_land() {
+    let player = Player::new();
+    let hand_with_land = Hand::new("Mountain");
+    let state = GameState::new_with_hand(hand_with_land);
+    let new_state = player.play_turn(state);
+
+    assert!(new_state.battlefield.contains(&"Mountain".to_string()));
+    assert_eq!(1, new_state.turn);
 }
